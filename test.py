f559be8f4117414a2fdf90b445a2bc812ae7812d
oszn <oszn>
2022-06-21 20:18:41 +0800

    fix for my

---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py   
+@Contact :   1091756452@qq.com
+@License :   (C)Copyright 2022-2024
+ 
+@Desciption: 
+
+@Author: oszn(y.liu)
+@Modify Time    
+------------ 
+2022/6/21 18:22       
+'''
+
+from data.dataset import GeoDataset
+
+if __name__ == '__main__':
+    g=GeoDataset("","")
